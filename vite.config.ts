de5dc49b2e0731fc6f066f0e7e25b9b259102cfa
cristianreta <cristianreta2604@gmail.com>
2025-09-15 09:04:44 -0300

    feat: CORS agregado de host local ip

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,8 @@ import path from 'node:path'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: { alias: { '@': path.resolve(__dirname, 'src') } },
+  server: {
+    host: "0.0.0.0", // <- permite acceder con la IP de tu PC
+    port: 5173       // <- podés cambiarlo si querés
+  }
 })
